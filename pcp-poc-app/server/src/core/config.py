af42304ccfe0445fc556089a1c7969db2832daa6
gerald-eai <gerald@explore.ai>
2023-09-20 12:35:30 +0200

    Added ability to cache AAD tokens. Code cleanup and optimization.

--- pcp-poc-app/server/src/core/config.py
+++ pcp-poc-app/server/src/core/config.py
@@ -2,23 +2,19 @@ import os
 from dotenv import load_dotenv
 import os
 from pydantic import Field
-
-# from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import BaseSettings
 from functools import lru_cache
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
+from typing import Optional
 
+load_dotenv(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".env.local"))
 app_env = os.environ.get("APP_ENVIRONMENT", "local")
-if app_env == "local":
-    load_dotenv(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".env.local"))
-else:
-    # load the data from our .env file
-    load_dotenv(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".env.dev"))
-print(app_env)
 
 
 class Settings(BaseSettings):
+    APP_ENVIRONMENT: str = Field(env="APP_ENVIRONMENT")
+    # local settings
     DB_HOSTNAME: str = Field(env="DB_HOSTNAME")
     DB_NAME: str = Field(env="DB_NAME")
     DB_USER: str = Field(env="DB_USER")
@@ -27,26 +23,26 @@ class Settings(BaseSettings):
     DB_SCHEMA: str = Field(env="DB_SCHEMA")
     SQL_DRIVER: str = Field(env="SQL_DRIVER")
 
+    # dev settings
+    AZURE_TENANT_ID: str = Field(env="AZURE_TENANT_ID")
+    AZURE_USERNAME: str = Field(env="AZURE_USERNAME")
+    AZURE_PASSWORD: str = Field(env="AZURE_PASSWORD")
+    AZURE_CLIENT_ID: str = Field(env="AZURE_CLIENT_ID")
+    AZURE_CLIENT_SECRET: str = Field(env="AZURE_CLIENT_SECRET")
+    AZURE_DATABASE: str = Field(env="AZURE_DATABASE")
+    AZURE_HOSTNAME: str = Field(env="AZURE_HOSTNAME")
+    AZURE_SCHEMA: str = Field(env="AZURE_SCHEMA")
+
+    # sn keyvault values
+    SN_CLIENT_ID: Optional[str] = Field(env="SNServicePrincipleAppID")
+    SN_CLIENT_SECRET: Optional[str] = Field(env="SNServicePrinciple")
+    SN_HOSTNAME: Optional[str] = Field(env="DPlatSQLDB-HOST")
+    SN_DATABASE: Optional[str] = Field(env="DPlatSQLDB-Database")
+
     class config:
-        env_file = f".env.{os.getenv('APP_ENVIRONMENT', f'{app_env}')}"
+        env_file = f".env.local"
         env_file_encoding = "utf-8"
         case_sensitive = True
-
-
-class AzureKeyVaultSettings(BaseSettings):
-    # SN Settings
-    SN_CLIENT_ID: str = Field(env="SNServicePrincipleAppID")
-    SN_CLIENT_SECRET: str = Field(env="SNServicePrinciple")
-    DB_HOSTNAME: str = Field(env="DPlatSQLDB-HOST")
-    DB_NAME: str = Field(env="DPlatSQLDB-Database")
-    DB_USER: str = Field(env="z-one-username")
-    DB_PWD: str = Field(env="z-one-password")
-
-    # Regular DB settings
-    DB_SCHEMA: str = Field(env="DB_SCHEMA")
-    SQL_DRIVER: str = Field(env="SQL_DRIVER")
-    DB_PORT: str = Field(env="DB_PORT")
-    TENANT_ID: str = Field(env="TENANT_ID")
 
     @classmethod
     def load_from_sn_keyvault(cls):
@@ -57,14 +53,7 @@ class AzureKeyVaultSettings(BaseSettings):
         for field_name, field in cls.__fields__.items():
             field_info = field.field_info.extra.get("env")
             if field_info:
-                if (
-                    field_info == "DB_SCHEMA"
-                    or field_info == "SQL_DRIVER"
-                    or field_info == "TENANT_ID"
-                    or field_info == "DB_PORT"
-                ):
-                    continue
-                else:
+                if "SN_" in field_info:
                     secret_name = field.field_info.extra["env"]
                     secret = vault_client.get_secret(secret_name)
                     secret_value[field_name] = secret.value
@@ -79,4 +68,4 @@ def get_settings():
 
 @lru_cache(maxsize=10)
 def get_azure_keyvault_settings():
-    return AzureKeyVaultSettings.load_from_sn_keyvault()
+    return Settings.load_from_sn_keyvault()
--- pcp-poc-app/server/src/server.py
+++ pcp-poc-app/server/src/server.py
@@ -1,25 +1,34 @@
 from api.api import api_router, default_router
-from core import config as config_manager
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from core.app_startup import ProductConfigAPI
+from core.auth import get_cached_token
 
-api = FastAPI()
+api = FastAPI(title="Product Config Portal API Server", debug=True)
+# create a dependency to get the cached token
+def get_token():
+    token = get_cached_token()
+    if not token:
+        raise ValueError("Invalid AAD Token")
+    return token
 
 
 def initialize_fastapi_backend():
-    # settings = config_manager.get_settings()
-    azure_settings = config_manager.get_azure_keyvault_settings()
     api.mount("/static", StaticFiles(directory="static"), name="static")
     api.include_router(default_router, tags=["default"])
     api.include_router(api_router, tags=["api"])
-
     return api
 
 
 api: FastAPI = initialize_fastapi_backend()
 
+
+@api.on_event("startup")
+def startup_event():
+    api.state.token = get_token()
+
+
 if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run("server:api", host="127.0.0.1", port=8000, reload=True)
-
